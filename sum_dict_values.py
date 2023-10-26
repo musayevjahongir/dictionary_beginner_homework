876c0a96d1988cc484770e45585667472a80f8a0
musayevjahongir <musayevjahongir5051@gmail.com>
2023-10-26 21:52:47 +0500

    qiymatlar yig'indisi

--- sum_dict_values.py
+++ sum_dict_values.py
@@ -6,4 +6,20 @@ def sum_dict_values(data: dict) -> int:
     Returns:
         int: The sum of all values in the dictionary
     '''
-    return 
+    s=0
+    for i in data:
+        s+=data[i]
+    return s
+print(sum_dict_values( data = {
+    'a': 1, 
+    'b': 2, 
+    'c': 3
+  }))
+print(sum_dict_values(data = {
+    1: 23, 
+    2: 3.5, 
+    4: 1, 
+    6: 7, 
+    5: 2, 
+    7: 3
+  }))
