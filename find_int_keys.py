e5f43891f51cb325bd26afc95d8f495dfbc29570
musayevjahongir <musayevjahongir5051@gmail.com>
2023-10-26 17:03:31 +0500

    raqamli keylarni aniqlash

--- find_int_keys.py
+++ find_int_keys.py
@@ -6,4 +6,14 @@ def find_int_keys(data: dict) -> list:
     Returns:
         list: A list of all keys in the dictionary that are integers.
     """
-    return 
+    a=[]
+    for i in data:
+        if str(i).isdigit():
+            a.append(i)
+    return a
+print(find_int_keys(data = {
+    'a': 1, 
+    3 : 2, 
+    'c': 3,
+    10:'a'
+  }))
