01954586229f85a6319c9d109abf58048549c562
musayevjahongir <musayevjahongir5051@gmail.com>
2023-10-26 22:16:39 +0500

    floatlar yig'indisi

--- sum_float_values.py
+++ sum_float_values.py
@@ -6,4 +6,21 @@ def sum_float_values(data: dict) -> float:
     Returns:
         float: The sum of all float values in the dictionary.
     '''
-    return 
+    s=0
+    for i in data:
+        if "." in str(data[i]):
+            s+=data[i]
+    return s
+print(sum_float_values(data = {
+    'a': 1, 
+    'b' : 2.5, 
+    'c': 3.0
+  }))
+print(sum_float_values(data = {
+    1: 22.4, 
+    2: 3.5, 
+    4: 1, 
+    6: 7.6, 
+    5: 2, 
+    7: 3
+  }))
