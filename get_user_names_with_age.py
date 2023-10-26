40eed7da168f6635f482608b825e8f3edf7bc5c8
musayevjahongir <musayevjahongir5051@gmail.com>
2023-10-26 21:37:43 +0500

    yoshga tengmi

--- get_user_names_with_age.py
+++ get_user_names_with_age.py
@@ -8,4 +8,38 @@ def get_user_names_with_age(data:list, age:int) -> list:
     Returns:
         list: A list of users with the given age
     """
-    return 
+    a=[]
+    for i in data:
+        if i["age"]==age:
+            a.append(i["name"])
+    return a
+print(get_user_names_with_age(data = [
+  {
+    'name': 'John', 
+    'age': 27
+  }, 
+  {
+    'name': 'Mary', 
+    'age': 42
+  }
+],
+age = 27))
+print(get_user_names_with_age(data = [
+  {
+    'name': 'John', 
+    'age': 30
+  }, 
+  {
+    'name': 'Ann', 
+    'age': 32
+  }, 
+  {
+    'name': 'Sam', 
+    'age': 27
+  }, 
+  {
+    'name': 'Mary', 
+    'age': 32
+  }
+],
+age = 32))
