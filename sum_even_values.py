9c2091524cce1ae306be5cde06081608c34f1c75
musayevjahongir <musayevjahongir5051@gmail.com>
2023-10-26 21:57:13 +0500

    juft qiymatlar yig'indisi

--- sum_even_values.py
+++ sum_even_values.py
@@ -6,4 +6,21 @@ def sum_even_values(data: dict) -> int:
     Returns:
         int: The sum of all even values in the dictionary
     '''
-    return 
+    s=0
+    for i in data:
+        if data[i]%2==0:
+            s+=data[i]
+    return s
+print(sum_even_values(data = {
+    'a': 1, 
+    'b': 2, 
+    'c': 3
+  }))
+print(sum_even_values(data = {
+    1: 22, 
+    2: 3.5, 
+    4: 1, 
+    6: 7, 
+    5: 2, 
+    7: 3
+  }))
