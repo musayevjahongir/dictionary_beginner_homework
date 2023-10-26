cc3c5b9432612a5e3ff97286141de25d21eea75c
musayevjahongir <musayevjahongir5051@gmail.com>
2023-10-26 21:42:35 +0500

    ismni topish

--- get_user_names.py
+++ get_user_names.py
@@ -8,4 +8,46 @@ def get_user_names(data:list, country:str) -> list:
     Returns:
         list: A list of users with the given country
     """
-    return 
+    a=[]
+    for i in data:
+        if i ["country"]==country:
+            a.append(i["name"])
+    return a
+print(get_user_names(data = [
+  {
+    'name': 'John', 
+    'country': 'USA'
+  }, 
+  {
+    'name': 'Mary', 
+    'country': 'UK'
+  }
+],
+country = "USA"))
+print(get_user_names(data = [
+  {
+    'name': 'John', 
+    'country': 'USA'
+  }, 
+  {
+    'name': 'Mary', 
+    'country': 'UK'
+  },
+  {
+    'name': 'Henry', 
+    'country': 'UK'
+  },
+  {
+    'name': 'Sam', 
+    'country': 'MEX'
+  },
+  {
+    'name': 'Kevin', 
+    'country': 'RUS'
+  },
+  {
+    'name': 'Dustin', 
+    'country': 'GER'
+  }
+],
+country = 'UK'))
